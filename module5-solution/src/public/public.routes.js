031f8ea81d7938f21196430dbb71fc5f580283ea
Rosencruez <phuongdavidnguyen@gmail.com>
2017-01-22 17:09:08 -0500

    added images

--- module5-solution/src/public/public.routes.js
+++ module5-solution/src/public/public.routes.js
@@ -40,6 +40,12 @@ function routeConfig ($stateProvider) {
           return MenuService.getMenuItems($stateParams.category);
         }]
       }
+    })
+    .state('public.signup', {
+      url: '/signup',
+      templateUrl: 'src/public/signUp/sign-up.template.html',
+      controller: 'SignUpController',
+      controllerAs: 'signCtrl'
     });
 }
 })();
