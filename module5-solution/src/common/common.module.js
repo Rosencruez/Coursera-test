279be30109610007f51994d5eca1b46f5d33b3d4
Rosencruez <phuongdavidnguyen@gmail.com>
2017-01-21 21:04:12 -0500

    changed constant

--- module5-solution/src/common/common.module.js
+++ module5-solution/src/common/common.module.js
@@ -2,7 +2,7 @@
 "use strict";
 
 angular.module('common', [])
-.constant('ApiPath', 'https://ychaikin-course5.herokuapp.com')
+.constant('ApiPath', 'https://rosencruez.github.io/Coursera-test/module5-solution')
 .config(config);
 
 config.$inject = ['$httpProvider'];
